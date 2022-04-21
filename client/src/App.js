73e799913665d4bfb9ce072a181809e3bb22ab56
thong <leethong1997@gmail.com>
2022-04-21 15:12:53 +0700

    ad client

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Footer from "./layout/Footer/footer";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
